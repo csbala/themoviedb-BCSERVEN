80f95ec2c1a95d2218f983376b40ba916ca57348
csbala <50990890+csbala@users.noreply.github.com>
2025-03-06 11:18:43 +0100

    updates

--- page-objects/homePage.pom.ts
+++ page-objects/homePage.pom.ts
@@ -1,35 +1,36 @@
-import { Locator, Page } from '@playwright/test';
+import { Locator, Page } from "@playwright/test";
 
 export class HomePage {
-    private page: Page;
-    public readonly tmdbHeaderLocator: Locator
-    public readonly loginButtonLocator: Locator
-    public readonly searchBarLocator: Locator
-    public readonly searchButtonLocator: Locator
-  
+  private page: Page;
+  public readonly tmdbHeaderLocator: Locator;
+  public readonly loginButtonLocator: Locator;
+  public readonly searchBarLocator: Locator;
+  public readonly searchButtonLocator: Locator;
+
   constructor(page: Page) {
     this.page = page;
-    this.tmdbHeaderLocator = page.locator('a.logo  img[alt="The Movie Database (TMDB)"]');
+    this.tmdbHeaderLocator = page.locator(
+      'a.logo  img[alt="The Movie Database (TMDB)"]'
+    );
     this.loginButtonLocator = page.locator('a[aria-label="Login"]');
-    this.searchBarLocator = page.locator('#inner_search_v4');
+    this.searchBarLocator = page.locator("#inner_search_v4");
     this.searchButtonLocator = page.locator('input[value="Search"]');
   }
 
   async goto() {
-    await this.page.goto('/');
+    await this.page.goto("/");
   }
 
-  async typeTextIntoSearchbar(text: string){
+  async typeTextIntoSearchbar(text: string) {
     await this.searchBarLocator.fill(text);
   }
 
-  async clickSearchButton(){
+  async clickSearchButton() {
     await this.searchButtonLocator.click();
-}
- 
-async clickLoginButton(){
+  }
+
+  async clickLoginButton() {
     await this.loginButtonLocator.click();
     await this.page.waitForURL(/.*login/);
+  }
 }
-
-}  
